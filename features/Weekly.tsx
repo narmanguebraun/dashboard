2c248277eb4d3484ff929450a65f9c8ebdde4de2
narmanguebraun <narmanguebraun1@gmail.com>
2024-10-11 11:03:43 +0200

    Sort classes

--- features/Weekly.tsx
+++ features/Weekly.tsx
@@ -2,9 +2,9 @@ import Board from "@/components/Board";
 
 export default function Weekly() {
   return (
-    <div className="max-w-7xl m-auto flex justify-center">
+    <div className="m-auto flex max-w-7xl justify-center">
       <div>
-        <h1 className="text-center text-sm p-6">Weekly</h1>
+        <h1 className="p-6 text-center text-sm">Weekly</h1>
         <Board />
       </div>
     </div>
